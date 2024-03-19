81deff2a095556e5bc2d1f2855f7c4eb3b59ef03
AndrejTica <andrejtica@gmail.com>
2024-03-19 20:27:01 +0100

    helooooo

--- desktop-app/src/constants.ts
+++ desktop-app/src/constants.ts
@@ -2,7 +2,7 @@ export const AMAZONS = [
     { name: "Najlepsza", url: "best" },
     { name: "Niemcy", url: "https://www.amazon.de" },
     { name: "Wielka Brytania", url: "https://www.amazon.co.uk" },
-    // { name: "Włochy", url: "https://www.amazon.it" },
+    { name: "Włochy", url: "https://www.amazon.it" },
     { name: "Francja", url: "https://www.amazon.fr" },
     { name: "Hiszpania", url: "https://www.amazon.es" },
 ];
